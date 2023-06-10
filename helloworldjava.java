05fdc5dbd4dee5d2d386781d1f3549a3c8a4ffb2
dattiashok <96079247+dattiashok@users.noreply.github.com>
2023-06-10 15:36:48 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps to test webhook from webhook test 1!!");
+    System.out.println("Hello World from DevOps to test webhook from webhook test 2!!");
     }
 }
 
